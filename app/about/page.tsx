20f0a6c8e08bc24a6cbcbcd06eb892f2e33b3b52
Edede Godspower Aniekan <111289218+Adnom001@users.noreply.github.com>
2025-10-07 08:26:59 -0700

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,27 @@
-import About from "@/components/about/About";
-import Mission from "@/components/about/Mission";
-import Waitlist from "@/components/Waitlist";
+import Features from "@/components/home/Features";
+import FounderCard from "@/components/home/FounderCard";
+import Hero from "@/components/home/Hero";
+import Arrivals from "@/components/home/Arrivals";
+import Wl from "@/components/home/Wl";
+import Curated from "@/components/home/Curated";
+import FollowUs from "@/components/home/FollowUs";
+import Testimony from "@/components/home/Testimony";
 
-const page = () => {
-	return (
-		<div>
-			<About />
-			<Mission />
-			<Waitlist />
-		</div>
-	);
-};
-export default page;
+
+
+function Page() {
+    return (
+        <>
+            <Hero />
+            <Features />
+            <Curated />
+            <FounderCard />
+            <Arrivals />
+            <Testimony />
+            <FollowUs />
+            <Wl />
+        </>
+    );
+}
+
+export default Page;
